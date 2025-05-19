1ce0235708e5c8f538efef4f871ff9ca7eebaa02
shivamkorade <prkorade@gmail.com>
2025-05-19 23:05:29 +0530

    Updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Container, Row, Col, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/alumni.png";
 import projImg2 from "../assets/img/IXplorer.png";
